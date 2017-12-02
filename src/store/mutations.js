e26236995951ab8852f953655d40943dc7934715
wangxiang <261676607@qq.com>
2017-12-02 15:17:20 +0800

    vuex

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,23 +1,43 @@
-export const SET_SINGER = 'SET_SINGER'
 
-export const SET_PLAYING_STATE = 'SET_PLAYING_STATE'
+import * as types from './mutation-types'
 
-export const SET_FULL_SCREEN = 'SET_FULL_SCREEN'
+const mutations = {
+  [types.SET_SINGER](state, singer) {
+    state.singer = singer
+  },
+  [types.SET_PLAYING_STATE](state, flag) {
+    state.playing = flag
+  },
+  [types.SET_FULL_SCREEN](state, flag) {
+    state.fullScreen = flag
+  },
+  [types.SET_PLAYLIST](state, list) {
+    state.playlist = list
+  },
+  [types.SET_SEQUENCE_LIST](state, list) {
+    state.sequenceList = list
+  },
+  [types.SET_PLAY_MODE](state, mode) {
+    state.mode = mode
+  },
+  [types.SET_CURRENT_INDEX](state, index) {
+    state.currentIndex = index
+  },
+  [types.SET_DISC](state, disc) {
+    state.disc = disc
+  },
+  [types.SET_TOP_LIST](state, topList) {
+    state.topList = topList
+  },
+  [types.SET_SEARCH_HISTORY](state, history) {
+    state.searchHistory = history
+  },
+  [types.SET_PLAY_HISTORY](state, history) {
+    state.playHistory = history
+  },
+  [types.SET_FAVORITE_LIST](state, list) {
+    state.favoriteList = list
+  }
+}
 
-export const SET_PLAYLIST = 'SET_PLAYLIST'
-
-export const SET_SEQUENCE_LIST = 'SET_SEQUENCE_LIST'
-
-export const SET_PLAY_MODE = 'SET_PLAY_MODE'
-
-export const SET_CURRENT_INDEX = 'SET_CURRENT_INDEX'
-
-export const SET_DISC = 'SET_DISC'
-
-export const SET_TOP_LIST = 'SET_TOP_LIST'
-
-export const SET_SEARCH_HISTORY = 'SET_SEARCH_HISTORY'
-
-export const SET_PLAY_HISTORY = 'SET_PLAY_HISTORY'
-
-export const SET_FAVORITE_LIST = 'SET_FAVORITE_LIST'
+export default mutations
