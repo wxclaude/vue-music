82f6b5c9905f57035da7d38d77111a79eb7387fe
wangxiang <261676607@qq.com>
2018-01-05 16:35:30 +0800

    优化

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -2,25 +2,19 @@
   <div class="slider" ref="slider">
     <div class="slider-group" ref="sliderGroup">
       <slot>
-
       </slot>
     </div>
     <div class="dots">
-      <span class="dot" v-for="(item, index) in dots" :class="{active:currentPageIndex === index}"></span>
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots"></span>
     </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import BScroll from 'better-scroll'
   import {addClass} from 'common/js/dom'
+  import BScroll from 'better-scroll'
   export default {
-    data() {
-      return {
-        dots: [],
-        currentPageIndex: 0
-      }
-    },
+    name: 'slider',
     props: {
       loop: {
         type: Boolean,
@@ -32,7 +26,13 @@
       },
       interval: {
         type: Number,
-        default: 2000
+        default: 4000
+      }
+    },
+    data() {
+      return {
+        dots: [],
+        currentPageIndex: 0
       }
     },
     mounted() {
@@ -45,14 +45,46 @@
         }
       }, 20)
       window.addEventListener('resize', () => {
-        if (!this.slider) {
+        if (!this.slider || !this.slider.enabled) {
           return
         }
-        this._setSliderWidth(true)
-        this.slider.refresh()
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+          if (this.slider.isInTransition) {
+            this._onScrollEnd()
+          } else {
+            if (this.autoPlay) {
+              this._play()
+            }
+          }
+          this.refresh()
+        }, 60)
       })
     },
+    activated() {
+      this.slider.enable()
+      let pageIndex = this.slider.getCurrentPage().pageX
+      this.slider.goToPage(pageIndex, 0, 0)
+      this.currentPageIndex = pageIndex
+      if (this.autoPlay) {
+        this._play()
+      }
+    },
+    deactivated() {
+      this.slider.disable()
+      clearTimeout(this.timer)
+    },
+    beforeDestroy() {
+      this.slider.disable()
+      clearTimeout(this.timer)
+    },
     methods: {
+      refresh() {
+        if (this.slider) {
+          this._setSliderWidth(true)
+          this.slider.refresh()
+        }
+      },
       _setSliderWidth(isResize) {
         this.children = this.$refs.sliderGroup.children
         let width = 0
@@ -68,9 +100,6 @@
         }
         this.$refs.sliderGroup.style.width = width + 'px'
       },
-      _initDots() {
-        this.dots = new Array(this.children.length)
-      },
       _initSlider() {
         this.slider = new BScroll(this.$refs.slider, {
           scrollX: true,
@@ -82,34 +111,39 @@
             speed: 400
           }
         })
-        this.slider.on('scrollEnd', () => {
-          let pageIndex = this.slider.getCurrentPage().pageX
-          if (this.loop) {
-            pageIndex -= 1
-          }
-          this.currentPageIndex = pageIndex
-
+        this.slider.on('scrollEnd', this._onScrollEnd)
+        this.slider.on('touchend', () => {
           if (this.autoPlay) {
-            clearTimeout(this.timer)
             this._play()
           }
         })
+        this.slider.on('beforeScrollStart', () => {
+          if (this.autoPlay) {
+            clearTimeout(this.timer)
+          }
+        })
+      },
+      _onScrollEnd() {
+        let pageIndex = this.slider.getCurrentPage().pageX
+        this.currentPageIndex = pageIndex
+        if (this.autoPlay) {
+          this._play()
+        }
+      },
+      _initDots() {
+        this.dots = new Array(this.children.length)
       },
       _play() {
-        let pageIndexA = this.slider.getCurrentPage().pageX + 1
         clearTimeout(this.timer)
         this.timer = setTimeout(() => {
-          this.slider.goToPage(pageIndexA, 0, 400)
+          this.slider.next()
         }, this.interval)
       }
-    },
-    destroyed() {
-      clearTimeout(this.timer)
     }
   }
 </script>
 
-<style lang="stylus" rel="stylesheet/stylus">
+<style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
   .slider
     min-height: 1px
