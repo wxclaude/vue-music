118beb8eaefc0b7d6827b97d898ebb1a01218590
wangxiang <261676607@qq.com>
2018-01-05 16:27:28 +0800

    添加获取歌曲列表

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,46 +1,88 @@
-import { commonParams, options } from './config'
-import axios from 'axios'
-import jsonp from 'common/js/jsonp'
-import { getUid } from 'common/js/uid'
+import {getLyric, getVKey} from 'api/song'
+import {getUid} from './uid'
+import {ERR_OK} from 'api/config'
+import {Base64} from 'js-base64'
 
-const debug = process.env.NODE_ENV !== 'production'
+let urlMap = {}
 
-export function getLyric(mid) {
-  const url = debug ? '/api/lyric' : 'http://ustbhuangyi.com/music/api/lyric'
+export default class Song {
+  constructor({id, mid, singer, name, album, duration, image, url}) {
+    this.id = id
+    this.mid = mid
+    this.singer = singer
+    this.name = name
+    this.album = album
+    this.duration = duration
+    this.image = image
+    this.filename = `C400${this.mid}.m4a`
+    // 确保一首歌曲的 id 只对应一个 url
+    if (urlMap[this.id]) {
+      this.url = urlMap[this.id]
+    } else {
+      if (url) {
+        this.url = url
+        urlMap[this.id] = url
+      } else {
+        this._genUrl()
+      }
+    }
+  }
 
-  const data = Object.assign({}, commonParams, {
-    songmid: mid,
-    platform: 'yqq',
-    hostUin: 0,
-    needNewCode: 0,
-    categoryId: 10000000,
-    pcachetime: +new Date(),
-    format: 'json'
-  })
+  getLyric() {
+    if (this.lyric) {
+      return Promise.resolve(this.lyric)
+    }
 
-  return axios.get(url, {
-    params: data
-  }).then((res) => {
-    return Promise.resolve(res.data)
+    return new Promise((resolve, reject) => {
+      getLyric(this.mid).then((res) => {
+        if (res.retcode === ERR_OK) {
+          this.lyric = Base64.decode(res.lyric)
+          resolve(this.lyric)
+        } else {
+          // eslint-disable-next-line
+          reject('no lyric')
+        }
+      })
+    })
+  }
+
+  _genUrl() {
+    if (this.url) {
+      return
+    }
+    getVKey(this.mid, this.filename).then((res) => {
+      if (res.code === ERR_OK) {
+        const vkey = res.data.items[0].vkey
+        this.url = `http://dl.stream.qqmusic.qq.com/${this.filename}?vkey=${vkey}&guid=${getUid()}&uin=0&fromtag=66`
+        urlMap[this.id] = this.url
+      }
+    })
+  }
+}
+
+export function createSong(musicData) {
+  return new Song({
+    id: musicData.songid,
+    mid: musicData.songmid,
+    singer: filterSinger(musicData.singer),
+    name: musicData.songname,
+    album: musicData.albumname,
+    duration: musicData.interval,
+    image: `https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`
   })
 }
 
-export function getVKey(songmid, filename) {
-  const url = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg'
-
-  const data = Object.assign({}, commonParams, {
-    cid: 205361747,
-    format: 'json',
-    platform: 'yqq',
-    hostUin: 0,
-    needNewCode: 0,
-    uin: 0,
-    songmid,
-    filename,
-    guid: getUid()
+function filterSinger(singer) {
+  let ret = []
+  if (!singer) {
+    return ''
+  }
+  singer.forEach((s) => {
+    ret.push(s.name)
   })
+  return ret.join('/')
+}
 
-  return jsonp(url, data, Object.assign({}, options, {
-    param: 'callback'
-  }))
+export function isValidMusic(musicData) {
+  return musicData.songid && musicData.albummid && (!musicData.pay || musicData.pay.payalbumprice === 0)
 }
