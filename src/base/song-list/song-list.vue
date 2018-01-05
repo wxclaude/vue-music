a52e9f40339979112e9ae03df9fe31f045da876e
wangxiang <261676607@qq.com>
2018-01-05 16:34:11 +0800

    导入Mixin

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,30 +1,49 @@
 <template>
-  <div class="search-list" v-show="searches.length">
-    <transition-group name="list" tag="ul">
-      <li :key="item" class="search-item" @click="selectItem(item)" v-for="item in searches">
-        <span class="text">{{item}}</span>
-        <span class="icon" @click.stop="deleteOne(item)">
-          <i class="icon-delete"></i>
-        </span>
+  <div class="song-list">
+    <ul>
+      <li @click="selectItem(song, index)" class="item" v-for="(song, index) in songs">
+        <div class="rank" v-show="rank">
+          <span :class="getRankCls(index)" v-text="getRankText(index)"></span>
+        </div>
+        <div class="content">
+          <h2 class="name">{{song.name}}</h2>
+          <p class="desc">{{getDesc(song)}}</p>
+        </div>
       </li>
-    </transition-group>
+    </ul>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
     props: {
-      searches: {
+      songs: {
         type: Array,
         default: []
+      },
+      rank: {
+        type: Boolean,
+        default: false
       }
     },
     methods: {
-      selectItem(item) {
-        this.$emit('select', item)
+      selectItem(item, index) {
+        this.$emit('select', item, index)
       },
-      deleteOne(item) {
-        this.$emit('delete', item)
+      getDesc(song) {
+        return `${song.singer}·${song.album}`
+      },
+      getRankCls(index) {
+        if (index <= 2) {
+          return `icon icon${index}`
+        } else {
+          return 'text'
+        }
+      },
+      getRankText(index) {
+        if (index > 2) {
+          return index + 1
+        }
       }
     }
   }
@@ -33,22 +52,41 @@
 <style scoped lang="stylus" rel="stylesheet/stylus">
   @import "~common/stylus/variable"
   @import "~common/stylus/mixin"
-  .search-list
-    .search-item
+  .song-list
+    .item
       display: flex
       align-items: center
-      height: 40px
-      overflow: hidden
-      &.list-enter-active, &.list-leave-active
-        transition: all 0.1s
-      &.list-enter, &.list-leave-to
-        height: 0
-      .text
+      box-sizing: border-box
+      height: 64px
+      font-size: $font-size-medium
+      .rank
+        flex: 0 0 25px
+        width: 25px
+        margin-right: 30px
+        text-align: center
+        .icon
+          display: inline-block
+          width: 25px
+          height: 24px
+          background-size: 25px 24px
+          &.icon0
+            bg-image('first')
+          &.icon1
+            bg-image('second')
+          &.icon2
+            bg-image('third')
+        .text
+          color: $color-theme
+          font-size: $font-size-large
+      .content
         flex: 1
-        color: $color-text-l
-      .icon
-        extend-click()
-        .icon-delete
-          font-size: $font-size-small
+        line-height: 20px
+        overflow: hidden
+        .name
+          no-wrap()
+          color: $color-text
+        .desc
+          no-wrap()
+          margin-top: 4px
           color: $color-text-d
 </style>
